424dea2948534b66e2b5d9caa41e2a03f615be2e
Atonu <susnigdho1234@gmail.com>
2026-09-23 15:30:26 +0600

    Add Profile and Skills models with migrations; update admin registration and enhance profile setup template

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Profile)
+admin.site.register(Skills)
 
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Skills(models.Model):
+    name = models.CharField(max_length=255)
+    
+    
+    def __str__(self) :
+        return self.name
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='images',blank=True, null=True, default="/static/images/default.svg")
+    first_name = models.CharField( max_length=50)
+    last_name = models.CharField( max_length=50)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    institute = models.CharField(max_length=100, blank=True, null=True)
+    url = models.TextField()
+    skills = models.ManyToManyField(Skills)
+    
+    
+    
+    def __str__(self) :
+        return self.user.username
+    
+
+    
+    
+
+    
